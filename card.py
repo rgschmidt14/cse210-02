372dc59e6c9d3c5d74e30a12e427a85f7c85fc56
Alvarobyui <u701860138@byui.edu>
2022-01-29 23:32:46 -0500

    Final details in card.py

--- card.py
+++ card.py
@@ -16,12 +16,12 @@ class Card:
     
     def newNumber(self):
         """Generates a new number"""
-        self.value = random.randint(1, 12)
+        self.value = random.randint(1, 13)
         print(f"The Card is: {self.value} ")
 
         """Generates a second random number to compare with the first one"""
     def nextNumber(self):
-        self.nextValue = random.randint(1, 12)
+        self.nextValue = random.randint(1, 13)
         print(f"Next Card was: {self.nextValue} ")
 
  
